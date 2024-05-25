cce46d74a49cfeef4663b97408df7f7937cbfa81
SultanEsen <beksultanesenkulov@MacBook-Air--Beksultan.local>
2024-05-25 15:55:28 +0600

    main page, categories added

--- bboard/models.py
+++ bboard/models.py
@@ -2,8 +2,28 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=50, verbose_name='Product')
+    content = models.TextField(null=True, blank=True, verbose_name='Description')
+    price = models.FloatField(null=True, blank=True, verbose_name='Price')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='When published')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Rubric')
 
+    class Meta:
+        verbose_name_plural = 'Announcements'
+        verbose_name = 'Announcement'
+        ordering = ['-published']
+
+    def __str__(self):
+        return self.title
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Title')
+
+    class Meta:
+        verbose_name_plural = 'Rubrics'
+        verbose_name = 'Rubric'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
